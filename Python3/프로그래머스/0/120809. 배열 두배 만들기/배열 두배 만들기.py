d28db0c558efefe3578192d38b474544c810072a
rkddpfls02 <140104885+rkddpfls02@users.noreply.github.com>
2024-07-23 02:55:36 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ Python3/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,5 +1,3 @@
+# List Comprension- [수식 for i in 반복가능객체 if] or [수식 if else for i in 반복가능객체]
 def solution(numbers):
-    answer = []
-    for num in numbers:
-        answer.append(num*2)
-    return answer
+    return [num*2 for num in numbers]
