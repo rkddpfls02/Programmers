b63b841e3e763a538a3dca2ee8a4e3c6606d1025
rkddpfls02 <140104885+rkddpfls02@users.noreply.github.com>
2024-08-04 02:13:53 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ Python3/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,7 +1,5 @@
 def solution(my_string, num1, num2):
     sarr= list(my_string)
-    temp= sarr[num1]
-    sarr[num1]= sarr[num2]
-    sarr[num2]= temp
+    sarr[num1], sarr[num2]= sarr[num2], sarr[num1]
     #요소들을 하나의 문자열로 합칠 때는 join() 메서드
     return ''.join(sarr)
