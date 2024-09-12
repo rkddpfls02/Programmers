e3271ccbca77c22ecdf592f52faa09435f929c90
rkddpfls02 <140104885+rkddpfls02@users.noreply.github.com>
2024-09-12 21:49:53 +0900

    [Silver IV] Title: 접미사 배열, Time: 156 ms, Memory: 16528 KB -BaekjoonHub

--- Java/백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ Java/백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -1,13 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main  {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
-        String s= bf.readLine();
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s= br.readLine();
         String[] suffix= new String[s.length()];
         for (int i=0; i<s.length(); i++){
             suffix[i]= s.substring(i);
@@ -15,7 +14,8 @@ public class Main  {
         Arrays.sort(suffix);
 
         for (int i=0; i<suffix.length; i++){
-            System.out.println(suffix[i]);
+            bw.write(suffix[i]+'\n');
         }
+        bw.flush();
     }
 }
