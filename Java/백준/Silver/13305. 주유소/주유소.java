04477fc1d05a317c780df92c04859c58836afed3
rkddpfls02 <140104885+rkddpfls02@users.noreply.github.com>
2024-12-01 03:39:15 +0900

    [Silver III] Title: 주유소, Time: 304 ms, Memory: 33852 KB, Score: 58 point -BaekjoonHub

--- Java/백준/Silver/13305. 주유소/주유소.java
+++ Java/백준/Silver/13305. 주유소/주유소.java
@@ -2,21 +2,6 @@ import java.io.*;
 
 public class Main {
 
-    class City implements Comparable<City>{
-        int cost;
-        int meter;
-
-        City(int cost, int meter){
-            this.cost= cost;
-            this.meter= meter;
-        }
-
-        @Override
-        public int compareTo(City o) {
-            return this.cost-o.cost;
-        }
-    }
-
     public void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
@@ -24,34 +9,26 @@ public class Main {
         String[] meter= br.readLine().split(" ");
         String[] cost= br.readLine().split(" ");
 
+        int[] distance= new int[n-1];
+        int[] costs= new int[n-1];
+
+
         // 마지막 도시는 어짜피 도착지니까 무시
-        City[] citys= new City[n-1];
         int ans=0; // 총 가격
 
         for( int i=0; i<n-1; i++) {
-            citys[i] = new City(Integer.parseInt(cost[i]),Integer.parseInt(meter[i]));
+            distance[i]= Integer.parseInt(meter[i]);
+            costs[i]= Integer.parseInt(cost[i]);
         }
 
-        // 최소 cost 가진 도시 찾고 그 이후 meter 합 만큼 구매
-        // 그 뒤 모두 무한대로 바꿔놓고 이 과정 반복
-        // 무한대로 바꿔진 도시가 n-1이 되면 끝
-        int index= n-1; //마지막 최소 인덱스 첨엔 n-1
-        while(index!=0){
-            int min= Integer.MAX_VALUE;
-            int minindex=0; // 현재 최소 인덱스
-            for(int i=0; i<index; i++){
-                if( citys[i].cost <= min) {
-                    minindex= i;
-                    min= citys[i].cost;
+        // 최소 cost로 계속 구매
+        int min= costs[0];
+        for(int i=0; i<n-1; i++){
+                if(costs[i] <= min) {
+                    min= costs[i];
                 }
+                ans += min * distance[i];
             }
-
-            for(int i= minindex; i<index; i++) {
-                ans += min * citys[i].meter;
-            }
-
-            index= minindex;
-        }
 
         bw.write(Integer.toString(ans));
         bw.flush();
@@ -61,7 +38,5 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         new Main().solution();
-
-
     }
 }
