0c5852576dbed1abe3ee489e325516a25b93bdf4
rkddpfls02 <140104885+rkddpfls02@users.noreply.github.com>
2025-01-21 03:26:32 +0900

    [Silver I] Title: 기타 레슨, Time: 272 ms, Memory: 23780 KB -BaekjoonHub

--- Java/백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ Java/백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -3,62 +3,50 @@ import java.util.*;
 
 public class Main {
 
-    public void solution() throws IOException {
+    public void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st= new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken()); // 비디오 개수
-        int m = Integer.parseInt(st.nextToken()); // 블루레이 개수
+        int n= Integer.parseInt(st.nextToken()); // 강의 수
+        int m= Integer.parseInt(st.nextToken()); // 블루레이 수
 
-        st = new StringTokenizer(br.readLine());
-        List<Long> videos = new ArrayList<>();
-
-        long max = 0; // 비디오 길이 합
-        long min = 0; // 가장 긴 비디오의 길이
-
-        for (int i = 0; i < n; i++) {
-            long tmp = Long.parseLong(st.nextToken());
-            videos.add(tmp);
-            max += tmp;
-            min = Math.max(min, tmp); // 가장 긴 비디오의 길이
+        st= new StringTokenizer(br.readLine());
+        int[] videos= new int[n] ;
+        int min= 0, max=0;
+        for (int i=0; i<n; i++) {
+            int tmp=Integer.parseInt(st.nextToken());
+            if(min<tmp) min= tmp; // 최소는 가장 용량이 큰 비디오
+            max+= tmp;
+            videos[i]= tmp;
         }
+        int ans=0;
 
-        long result = max;
+        while(min <= max){
+            int mid= (min+max)/2;
+            int sum=0;
+            int blnum=1; //만들어진 블루레이 개수
 
-        // 이진 탐색
-        while (min <= max) {
-            long capacity = (min + max) / 2; // 블루레이 용량
-            if (canDivide(videos, capacity, m)) {
-                result = capacity;
-                max = capacity - 1;
-            } else {
-                min = capacity + 1;
+            for(int video: videos) {
+                if(sum+ video > mid) {
+                    sum=video;
+                    blnum++;
+                }
+                else sum += video;
             }
+
+            // lower bound
+            if(blnum<= m)  {
+                max= mid-1; // m보다 작으면 크기를 줄여야함- 크기가 m과 같다면 크기가 젤 작은걸로
+                ans= mid;
+            }
+            else min= mid+1; // m보다 크거나 같으면 크기를 늘려야함
         }
 
-        bw.write(Long.toString(result) + "\n");
+        bw.write(Integer.toString(ans));
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    // 주어진 용량(capacity)으로 m개의 블루레이로 나눌 수 있는지 확인하는 함수
-    private boolean canDivide(List<Long> videos, long capacity, int m) {
-        long sum = 0;
-        int count = 1; // 블루레이 개수
-
-        for (long video : videos) {
-            if (sum + video > capacity) {
-                count++;
-                sum = video; // 새로운 블루레이 시작
-                if (count > m) return false;
-            } else {
-                sum += video;
-            }
-        }
-
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
