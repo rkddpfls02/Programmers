17cb807178075ccab33f0e4bfed932d154380fe4
예린 <140104885+rkddpfls02@users.noreply.github.com>
2025-12-24 16:01:38 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.31 ms, Memory: 9.95 MB -BaekjoonHub

--- Python3/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ Python3/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,9 +1,2 @@
 def solution(n):
-    answer = ''
-    
-    for i in range(n):
-        if i%2==0:
-            answer+='수'
-        else :
-            answer+='박'
-    return answer
+    return ''.join(["수박"[i%2]for i in range(n)])
