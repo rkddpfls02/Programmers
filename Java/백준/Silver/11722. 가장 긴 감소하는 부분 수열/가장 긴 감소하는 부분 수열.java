c5e3dcc08ebc6f746900442cc8fe782bce440276
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-02-23 17:25:42 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 124 ms, Memory: 14540 KB -BaekjoonHub

---
+++ Java/백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n= Integer.parseInt(br.readLine());
+        int[] dp= new int[n];
+        int[] arr = new int[n];
+        int ans= 1;
+
+        String[] s = br.readLine().split(" ");
+        for(int i=0; i< n; i++) arr[i]= Integer.parseInt(s[i]);
+
+        dp[0]=1;
+        for(int i= 1; i< n; i++){
+            dp[i]=1;
+           for(int j= 0; j< i; j++) if(arr[j]> arr[i]) dp[i]= Math.max(dp[i], dp[j]+1);
+        }
+
+        for(int i=0; i<n; i++) ans= Math.max(ans, dp[i]);
+        bw.write(Integer.toString(ans));
+        bw.flush();
+
+    }
+
+    private static int hasMore(int[] arr, int i) {
+        for(int j= i+1; j< arr.length; j++) if(arr[j]< arr[i]) return j;
+        return -1;
+    }
+
+}
