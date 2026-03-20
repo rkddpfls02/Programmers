d0fe65abd1e5b056b356b3b24c1c1fe790b7a070
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-03-20 18:07:56 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- Java/백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ Java/백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,31 +1,34 @@
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int ans = 0;
-        int m = 1000000000;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // i에서부터 시작한 j길이 계단수 개수
-        int[][] stair = new int[10][n + 1];
-        
-        // 길이가 1 
-        for (int i = 0; i < 10; i++) {
-            stair[i][1] = 1; 
-        }
-        
-        // 길이가 2 이상
-        for (int j = 2; j <= n; j++) { 
-            for (int i = 0; i < 10; i++) {
-                if (i == 0) stair[i][j] = stair[1][j - 1] % m;
-                else if (i == 9) stair[i][j] = stair[8][j - 1] % m;
-                else stair[i][j] = (stair[i + 1][j - 1] + stair[i - 1][j - 1]) % m;
+        int n = Integer.parseInt(br.readLine());
+        long [][] dp= new long[n][10]; // 길이가 n인 계단 수
+
+        for(int i=1 ; i< 10; i++) dp[0][i]= 1;
+
+        for(int i=1; i<n; i++){ // n이 i+1일때
+            for(int j=0; j< 10; j++){ // j는 마지막 자리 수
+                if(j==0) dp[i][j]= dp[i-1][j+1];
+                else if(j==9) dp[i][j]= dp[i-1][j-1];
+                else dp[i][j]= (dp[i-1][j-1]+ dp[i-1][j+1])% 1000000000;
             }
         }
 
-        for (int i = 1; i < 10; i++) ans = (ans + stair[i][n]) % m;
+        long sum= 0;
+        for(int i=0; i< 10; i++) {
+            sum += dp[n-1][i];
+        }
 
-        System.out.println(ans);
+        bw.write(Long.toString(sum% 1000000000));
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
-}
+
+    }
