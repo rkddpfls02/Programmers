c92a30102329af3bc7aea241ba593c702162e230
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-04-07 15:16:54 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        
+        Map <String, Integer> map= new HashMap<>();
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1],0)+1);            
+        }
+        
+        int answer=1;
+        Iterator<String> it= map.keySet().iterator();
+        while(it.hasNext()){
+            String key= it.next();
+            answer *= map.get(key)+1;
+        }
+        
+        return answer-1;
+    }
+}
