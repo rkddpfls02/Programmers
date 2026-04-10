b5ba049c8a36b51ef3897fbf8b865f0fc1552324
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-04-10 14:26:28 +0900

    [level 1] Title: 모의고사, Time: 0.92 ms, Memory: 72.7 MB -BaekjoonHub

--- Java/프로그래머스/1/42840. 모의고사/모의고사.java
+++ Java/프로그래머스/1/42840. 모의고사/모의고사.java
@@ -6,45 +6,24 @@ class Solution {
         int[] pattern2= {2, 1, 2, 3, 2, 4, 2, 5};      
         int[] pattern3= {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};   
         
-        int[] cnt= new int [3];
-        
         int p1= 0;
         int p2= 0;
-        int p3= 0;
+        int p3= 0; 
         
-        for(int num: answers) {
+        for(int i=0; i< answers.length; i++) {
             
-            if(num == pattern1[p1]) cnt[0] ++;
-            
-            if(num == pattern2[p2]) cnt[1] ++;
-            
-            if(num == pattern3[p3]) cnt[2] ++;
-            
-            
-            if(p1< pattern1.length-1) p1 ++;
-            else p1= 0;
-            
-            if(p2< pattern2.length-1) p2 ++;
-            else p2= 0;
-            
-            if(p3< pattern3.length-1) p3++;
-            else p3= 0;
+            if(answers[i] == pattern1[i% pattern1.length]) p1 ++;
+            if(answers[i] == pattern2[i% pattern2.length]) p2 ++;
+            if(answers[i] == pattern3[i% pattern3.length]) p3 ++;
             
         }
-        
         
         List<Integer> tmp= new ArrayList<>();
-        int max= -1;
-        for(int i=0; i< cnt.length; i++){
-            
-            if(max < cnt[i]) {
-                max= cnt[i];
-                tmp.clear();
-                tmp.add(i+1);
-                }
-            
-            else if(max == cnt[i]) tmp.add(i+1);
-        }
+        int max= Math.max(p1, Math.max(p2, p3));
+        
+        if(p1 == max) tmp.add(1);
+        if(p2 == max) tmp.add(2);
+        if(p3 == max) tmp.add(3);
 
         int[] answer = new int [tmp.size()];
         for(int i=0; i< tmp.size(); i++) answer[i]= tmp.get(i);
