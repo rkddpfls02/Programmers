cde087ea7bf1a94d460939e3ba8775f24301ee01
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-04-14 15:17:38 +0900

    [level 2] Title: 피로도, Time: 5.88 ms, Memory: 75 MB -BaekjoonHub

--- Java/프로그래머스/2/87946. 피로도/피로도.java
+++ Java/프로그래머스/2/87946. 피로도/피로도.java
@@ -9,32 +9,26 @@ class Solution {
         visited= new boolean[dungeons.length];
         Arrays.fill(visited, false);
         this.dungeons= dungeons;
-            
-        for(int i=0; i< dungeons.length; i++){
-            dfs(i, 0, k);
-            visited[i]= false;
-        }
+    
+        dfs(0, k);
         
         return max;
     }
     
-    public void dfs(int now, int cnt, int k){
+    public void dfs(int cnt, int k){
         
-        // 방문한 던전 가능 여부 
-        visited[now] = true;
-        if(dungeons[now][0]<= k) {
-            cnt ++;
-            k-= dungeons[now][1];
+        for(int i=0; i< dungeons.length; i++) { // i 번째에서 시작한 탐험
             max= Math.max(max, cnt);
-        }
-        
-        // 다음 던전 방문
-        for(int i=0; i< dungeons.length; i++) {
-            
-            if(!visited[i] && i != now) {
-                dfs(i, cnt, k);
+
+            if(!visited[i] && dungeons[i][0]<= k) {
+                visited[i] = true;
+                
+                // 방문안하고 k가 조건을 만족했기에 
+                //다음 던전 다시 0부터 되는거 찾기 k의 상태가 바꼈으니까
+                dfs(cnt+1, k-dungeons[i][1]); 
                 visited[i] = false; // 백트래킹
             }
+            
         }
         
     }
