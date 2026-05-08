d937af63697c614062d580aa1cf74b7e0455e0c9
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-05-08 16:27:59 +0900

    [level 3] Title: 이중우선순위큐, Time: 36.04 ms, Memory: 116 MB -BaekjoonHub

--- Java/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ Java/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,72 +1,35 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        // 키와 값으로 관리
-        Queue<int[]> min_q= new PriorityQueue<>(11, (o1, o2) -> o1[1] - o2[1]);
-        Queue<int[]> max_q = new PriorityQueue<>(11, (o1, o2) -> o2[1] - o1[1]);
+        Queue<Integer> min_q= new PriorityQueue<>();
+        Queue<Integer> max_q = new PriorityQueue<>(Collections.reverseOrder());
         
-        Map<Integer, Boolean> used = new HashMap<>();
-        int lastKey= 0;
         
         for(String operation: operations){
             
             if(operation.startsWith("I")) {
-                used.put(lastKey, false); // 유효한 숫자
                 int num= Integer.parseInt(operation.split(" ")[1]);
-                min_q.offer(new int[] {lastKey, num});
-                max_q.offer(new int[] {lastKey, num});
-                lastKey++;
+                min_q.offer(num);
+                max_q.offer(num);
             }
             
             if(operation.equals("D 1")){
-               while (!max_q.isEmpty()) {
-                   int key= max_q.poll()[0];
-                   if(!used.get(key)) { // 사용안했으면 뺌
-                       used.put(key, true);
-                       break; // 하나 뺐으면 끝
-                   }
-               }
+                if(!max_q.isEmpty()) min_q.remove(max_q.poll());
             }
             
             if(operation.equals("D -1")){
-               while (!min_q.isEmpty()) {
-                   int key= min_q.poll()[0];
-                   if(!used.get(key)) {
-                       used.put(key, true);
-                       break;
-                   }
-               }
+                if(!min_q.isEmpty()) max_q.remove(min_q.poll());
             }
             
             
         }
         
-        int[] answer = new int [2];
         
-        if(!used.containsValue(false)) return new int []{0,0};
+        
+        if(min_q.isEmpty() && max_q.isEmpty()) return new int []{0,0};
         else{
-            int[] tmp;
-            
-            while (!max_q.isEmpty()) {
-                   tmp= max_q.poll();
-                    
-                   if(!used.get(tmp[0])) { 
-                       answer[0]= tmp[1];
-                       break;
-                   }
-               }
-            
-            while (!min_q.isEmpty()) {
-                   tmp = min_q.poll();
-                    
-                   if(!used.get(tmp[0])) { 
-                       answer[1]= tmp[1];
-                       break;
-                   }
-               }
+            return new int [] { max_q.poll(), min_q.poll()};       
         }
         
-        
-        return answer;
     }
 }
