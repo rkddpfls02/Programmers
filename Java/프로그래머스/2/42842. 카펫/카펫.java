938bd8b6e0e67fcdb6bdef5b4e98580c0ec0870e
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-08-04 15:10:16 +0900

    [level 2] Title: 카펫, Time: 666.07 ms, Memory: 78 MB -BaekjoonHub

--- Java/프로그래머스/2/42842. 카펫/카펫.java
+++ Java/프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int [2];
-        for(int w= 3; w<= (brown-2)/2; w++){ // 가로
-            for(int h= 3; h<= w; h++) { // 세로
-                int tmp= 2*(w+h)-4;
-                if( tmp == brown && brown+yellow == w*h) {
-                    answer[0]= w;
-                    answer[1]= h;
-                    return answer;
-                }
+        int max= Math.max(brown,yellow);
+        for(int i=3; i<= max; i++){
+            for(int j=3; j<= max; j++){
+                if((i+j== brown/2+2) && (i-2)*(j-2)== yellow) 
+                    return new int[]{ Math.max(i,j) , Math.min(i,j) };
             }
         }
         
-        return answer;
+        return null;
     }
 }
