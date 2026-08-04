8104a52a0e407cf5f885c0d71614a5c2991fc4fa
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-08-04 11:36:29 +0900

    [level 1] Title: 모의고사, Time: 0.64 ms, Memory: 84.4 MB -BaekjoonHub

--- Java/프로그래머스/1/42840. 모의고사/모의고사.java
+++ Java/프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,32 +1,26 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
         
-        int[] pattern1= {1, 2, 3, 4, 5};      
-        int[] pattern2= {2, 1, 2, 3, 2, 4, 2, 5};      
-        int[] pattern3= {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};   
+        int[] p1= { 1, 2, 3, 4, 5};
+        int[] p2= { 2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3= {  3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
         
-        int p1= 0;
-        int p2= 0;
-        int p3= 0; 
+        int[] s= new int [3];
         
-        for(int i=0; i< answers.length; i++) {
-            
-            if(answers[i] == pattern1[i% pattern1.length]) p1 ++;
-            if(answers[i] == pattern2[i% pattern2.length]) p2 ++;
-            if(answers[i] == pattern3[i% pattern3.length]) p3 ++;
-            
+        for(int i=0; i< answers.length; i++){
+            s[0]+= answers[i]== p1[i% p1.length]? 1:0;
+            s[1]+= answers[i]== p2[i% p2.length]? 1:0;
+            s[2]+= answers[i]== p3[i% p3.length]? 1:0;
         }
         
-        List<Integer> tmp= new ArrayList<>();
-        int max= Math.max(p1, Math.max(p2, p3));
+        List<Integer> list= new ArrayList<>();
+        int max=  Math.max(Math.max(s[0], s[1]),s[2]);
+        for(int i=0; i<3; i++) if(s[i]== max) list.add(i+1);
         
-        if(p1 == max) tmp.add(1);
-        if(p2 == max) tmp.add(2);
-        if(p3 == max) tmp.add(3);
-
-        int[] answer = new int [tmp.size()];
-        for(int i=0; i< tmp.size(); i++) answer[i]= tmp.get(i);
+        int[] answer= new int [list.size()];
+        for(int i=0; i< answer.length; i++) answer[i]= list.get(i);
         
         return answer;
     }
