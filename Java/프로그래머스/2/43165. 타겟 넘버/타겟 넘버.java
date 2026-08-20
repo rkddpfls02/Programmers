7d91007421643654e94f1d3133dfccab2f1d3a9a
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-08-20 09:13:05 +0900

    [level 2] Title: 타겟 넘버, Time: 3.69 ms, Memory: 85 MB -BaekjoonHub

--- Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,22 @@
-import java.util.*;
 class Solution {
-    public int answer= 0;
-    public int[] numbers;
-    public int target;
+    private static int [] numbers;
+    private static int target;
+    private static int answer;
     
-    public int solution(int[] numbers, int target) {
-        this.target= target;
-        this.numbers= numbers;
-        dfs(0,0);
-        return answer;
-    }
-    
-    public void dfs(int i, int total){
-        if (i== numbers.length) { // 마지막까지 더한 상태
-            if(total == target) answer ++;
+    private static void dfs(int depth, int sum){
+        if(depth >= numbers.length){
+            if(sum == target) answer ++;
             return;
         }
         
-        dfs(i+1, total+ numbers[i]);
-        dfs(i+1, total- numbers[i]);
-
+        dfs(depth+1, sum+ numbers[depth]);
+        dfs(depth+1, sum- numbers[depth]);
         
+    }
+    public int solution(int[] numbers, int target) {
+        this.numbers= numbers;
+        this.target= target;
+        dfs(0,0);
+        return answer;
     }
 }
