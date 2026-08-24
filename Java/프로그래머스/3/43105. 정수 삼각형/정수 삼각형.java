f9a3b8c00854379bd804463908d84d7d899eb6f5
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-08-24 21:48:47 +0900

    [level 3] Title: 정수 삼각형, Time: 13.61 ms, Memory: 65.1 MB -BaekjoonHub

--- Java/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ Java/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,21 +2,27 @@ import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         int n= triangle.length;
+        int[][] sums= new int[n][];
+        for(int i=0; i< n; i++) sums[i]= new int[triangle[i].length];
+                
+        sums[0][0]= triangle[0][0];
         
         for(int i=1; i< n; i++){
-            triangle[i][0] += triangle[i-1][0];
-            triangle[i][i] += triangle[i-1][i-1];
-            for(int j= 1; j< i; j++){
-               triangle[i][j] += Math.max(triangle[i-1][j-1], 
-                                          triangle[i-1][j]);
-            }
-        
+           for(int j=0; j< triangle[i].length; j++){
+               
+                // left- 왼쪽대각선
+                if(j >0) {
+                    sums[i][j]= Math.max(sums[i][j] ,sums[i-1][j-1]+triangle[i][j]);
+                }
+                // right- 그냥 바로 윗원소
+                if(j != triangle[i].length-1) {
+                    sums[i][j]= Math.max(sums[i][j], sums[i-1][j]+triangle[i][j]);
+                }
+               
+            } 
         }
         
-        int [] last= triangle[n-1];
-        
-        Arrays.sort(last);
-        
-        return last[n-1];
+        Arrays.sort(sums[n-1]);
+        return sums[n-1][n-1];
     }
 }
