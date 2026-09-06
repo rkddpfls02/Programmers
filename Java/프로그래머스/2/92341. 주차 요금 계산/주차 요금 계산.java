67cc079d052fe5793dba61727e228057779b7a54
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-09-06 14:23:10 +0900

    [level 2] Title: 주차 요금 계산, Time: 12.66 ms, Memory: 92.1 MB -BaekjoonHub

--- Java/프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ Java/프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -3,18 +3,21 @@ class Solution {
     public static class Car{
         int timeSum=0;
         boolean in= false;
-        int inH=0;
         int inM=0;
+        int totalFee=0;
+    }
+    
+    public static int recordTomin(String time){
+        int h= Integer.parseInt(time.split(":")[0]);
+        int m= Integer.parseInt(time.split(":")[1]);
+        return h*60 + m;
     }
     
     public int[] solution(int[] fees, String[] records) {
         
         // 차번호, 차 요금 Map
-        Map<String, Car> result= new HashMap<>();
-        
-        // 차번호 오름차순 하기 위한 임시 배열
-        List<int[]> tmp= new ArrayList<>();
-        
+        Map<String, Car> result= new TreeMap<>();
+
         Car car;
         for(String record: records){
             String[] s= record.split(" ");
@@ -28,17 +31,14 @@ class Solution {
                 }
                 
                 car.in= true;
-                car.inH= Integer.parseInt(s[0].split(":")[0]);
-                car.inM= Integer.parseInt(s[0].split(":")[1]);
+                car.inM= recordTomin(s[0]);
                 result.put(s[1], car);
             }
             else{
                 // 나갈때
                 car= result.get(s[1]);  
-                int outH= Integer.parseInt(s[0].split(":")[0]);
-                int outM= Integer.parseInt(s[0].split(":")[1]);
                 // 시간 계산
-                car.timeSum+= (outH-car.inH)*60+ outM-car.inM;
+                car.timeSum+= recordTomin(s[0])-car.inM;
                 car.in= false;
             }
             
@@ -46,13 +46,14 @@ class Solution {
         
         // 주차요금 계산
         for(String key : result.keySet()){
+            
             // 무조건 기본요금은 부과
             int sum= fees[1];
             car= result.get(key);
             
             // 출차 안했으면 23:59 기준으로 출차
             if(car.in){
-                car.timeSum+= (23-car.inH)*60+ 59-car.inM;
+                car.timeSum+= 23*60+59- car.inM;
             }
                 
             // 기준시간 넘은 경우만 추가요금
@@ -62,16 +63,16 @@ class Solution {
             }
                 
             // 차번호와 총요금 저장
-            tmp.add(new int[] {Integer.parseInt(key), sum});
+            car.totalFee= sum;
             }
         
-        // 차번호 기준 정렬
-        Collections.sort(tmp, (c1,c2)-> c1[0]-c2[0]);
+        int[] answer= new int [result.size()];
         
-        int[] answer= new int [tmp.size()];
-        for(int i=0; i< tmp.size(); i++){
-            answer[i]= tmp.get(i)[1];
+        int idx= 0;
+        for(String key: result.keySet()){
+            answer[idx++ ]= result.get(key).totalFee;
         }
+        
         return answer;
     }
 }
