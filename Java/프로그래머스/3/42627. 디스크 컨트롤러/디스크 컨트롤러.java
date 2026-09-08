9313f3ad7d0d811c31fb06dce34b24af87400c85
예린 <140104885+rkddpfls02@users.noreply.github.com>
2026-09-08 14:10:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.52 ms, Memory: 80.3 MB -BaekjoonHub

--- Java/프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ Java/프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,67 +1,53 @@
 import java.util.*;
-
 class Solution {
     
-    
-    static int now=0;
-    static int total=0;
-    static int cnt=0;
     public int solution(int[][] jobs) {
-        Queue<Process> q= new PriorityQueue<>();
-        
-        // 시작 시간 순으로 정렬
-        Arrays.sort(jobs, (o1,o2)->  o1[0]-o2[0] ); 
         int n= jobs.length;
         
+        Queue<int []> pq = new PriorityQueue<>((q1, q2)->{
+            if(q1[1]!= q2[1]) return q1[1]- q2[1];
+            if(q1[0]!= q2[0]) return q1[0]- q2[0];
+            return q1[2]- q2[2];
+        });
+        
+        // 들어온 순서대로 맞춤
+        Queue<int[]> idxJobs = new PriorityQueue<>((q1, q2)-> {
+            if(q1[0] != q2[0]) return q1[0]-q2[0];
+            return q1[1]-q2[1];
+        });
+        
+        // 기존의 jobs배열에 인덱스 필드 추가
         for(int i=0; i< n; i++){
-            
+            int[] idxJob= new int[3];
+            idxJob[0]= jobs[i][0];
+            idxJob[1]= jobs[i][1];
+            idxJob[2]= i;
+            idxJobs.offer(idxJob);
         }
-        
-        int qidx= 0;
-        
-        
-        while(cnt < n){
-            
-            // 프로세스 시작 전에 현재시간보다 start시간이 작은거 큐에 다 넣기
-            for(int i= qidx; i< n; i++){
                 
-            if(jobs[i][0]<= now) {
-                q.offer(new Process(i, jobs[i][0], jobs[i][1]));
-                qidx ++;
-            }
-            else break;
+        int time=0;
+        int total=0;
+        
+        for(int i=0; i< n; i++){
+            while(!idxJobs.isEmpty()){                
+                // 아직 시작시간이 되지 않으면 넣지 않음
+                if(idxJobs.peek()[0] <= time) pq.offer(idxJobs.poll());
+                else break;
             }
             
-            if(!q.isEmpty()) {
-                Process p = q.poll();
-                now += p.time;
-                total += now - p.start;
-                cnt++;
-            } else now = jobs[qidx][0];
+            int[] now;
             
-        } 
-        
-        return total / n;
-    }
-    
-    
-    static class Process implements Comparable<Process>{
-        int num;
-        int start;
-        int time;
-        
-        Process(int num, int start, int time){
-            this.num= num;
-            this.start= start;
-            this.time= time;
+            // 시작시간이 된 거 하나도 없으면 다음꺼 꺼냄
+            if(pq.isEmpty()) now= idxJobs.poll();
+            else now= pq.poll();
+            
+            // 현재시간보다 시작시간이 더 뒤면
+            if(time< now[0]) time= now[0];
+            
+            time+= now[1];
+            total += time -now[0];
         }
-        
-        public int compareTo(Process o){
-            if(this.time== o.time){
-                if(this.start== o.start) return this.num- o.num;
-                else return this.start- o.start;
-            } else return this.time- o.time;
-        }
-        
+
+        return total/n;
     }
 }
